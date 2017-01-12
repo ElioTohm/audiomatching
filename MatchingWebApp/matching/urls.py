98f3ad7b1fd4b3a2452f9604f0be6594f2a2a8e1
Elio Tohme <eliotohme@gmail.com>
2017-01-12 20:38:26 +0200

    fix url related to python server warnings

--- MatchingWebApp/matching/urls.py
+++ MatchingWebApp/matching/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/match/$', views.match, name='match'),
-    url(r'^/fingerprint/$', views.fingerprint, name='match'),
+    url(r'^match/$', views.match, name='match'),
+    url(r'^fingerprint/$', views.fingerprint, name='match'),
 ]
