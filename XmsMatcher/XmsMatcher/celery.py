f04bdcf4db8ed3a2b8450c41ef6bd88a7a912d5f
Elio Tohme <eliotohme@gmail.com>
2017-04-26 16:55:34 +0300

    added periodic tasks

--- XmsMatcher/XmsMatcher/celery.py
+++ XmsMatcher/XmsMatcher/celery.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 import os
+import time
+from pymongo import MongoClient
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'XmsMatcher.settings')
@@ -16,3 +19,18 @@ app.config_from_object('django.conf:settings')
 # Load task modules from all registered Django app configs.
 # app.autodiscover_tasks()
 
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 2mins.
+    sender.add_periodic_task(120.0, periodicAddData.s('MANAR', 2500, '21'), name='joe-congo 1')
+    sender.add_periodic_task(120.0, periodicAddData.s('ALJADEED', 2500, '20'), name='joe-congo 2')
+
+@app.task
+def periodicAddData(channel_name, confidence, client_name):
+    print 'joe-congo added' + client_name
+    client = MongoClient('localhost', 27017)
+    db = client['database']
+
+    db.records.insert_one({'channel_name': channel_name, 'client_id': client_name,
+                           'confidence': confidence, 'timestamp': time.time()})
+                           
