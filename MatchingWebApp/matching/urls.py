9e482e89e78e8348f1a6510943006a94e0f18129
Elio Tohme <eliotohme@gmail.com>
2017-04-27 10:08:01 +0300

    deleted old folder

--- MatchingWebApp/matching/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^match/$', views.match, name='match'),
-    url(r'^fingerprint/$', views.fingerprint, name='match'),
-]
