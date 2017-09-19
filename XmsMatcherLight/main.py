16d80afe4031f736216855c65d751da7892a4e85
elio_tohme <eliotohme@gmail.com>
2017-09-19 22:50:32 +0300

    refactoring

--- XmsMatcherLight/main.py
+++ XmsMatcherLight/main.py
@@ -39,7 +39,7 @@ api = Api(app)
 
 app.config.update(
     CELERY_BROKER_URL= 'pyamqp://xms:987456321rabbitmq@127.0.0.1:5672/xms',
-    CELERY_RESULT_BACKEND='mongodb://xmsmongodb:xms@Prro#123mongo127.0.0.1/database',
+    CELERY_RESULT_BACKEND='mongodb://xms:987456321mongo@127.0.0.1/celery',
     MONGO_DBNAME='database',
     MONGO_USERNAME='xmsmongodb',
     MONGO_PASSWORD='xms@Prro#123mongo',
