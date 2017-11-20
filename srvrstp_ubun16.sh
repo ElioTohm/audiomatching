500d604c94cf95596197ef8635d537e12b192fc9
elio_tohme <eliotohme@gmail.com>
2017-11-20 09:39:25 +0200

    added dependencie

--- srvrstp_ubun16.sh
+++ srvrstp_ubun16.sh
@@ -4,10 +4,10 @@ sudo apt install -y python-mysqldb python-tk python-scipy libasound-dev portaudi
 sudo apt install -y  ffmpeg libav-tools
 
 # Installation if django framework was in use
-sudo pip install pymongo celery django datetime djangorestframework markdown django-filter numpy scipy pyaudio paho-mqtt 
+# sudo pip install pymongo celery django datetime djangorestframework markdown django-filter numpy scipy pyaudio paho-mqtt 
 
 # Installation if flask framework was in use 
-sudo pip install pymongo celery datetime markdown numpy scipy pyaudio paho-mqtt flask flask-restful Flask-PyMongo gevent
+sudo pip install pymongo celery datetime markdown numpy scipy pyaudio paho-mqtt flask flask-restful Flask-PyMongo gevent tornado
 
 python manage.py migrate
 
