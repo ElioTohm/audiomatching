43e997cc65d94ad717817b0ace66d506dcaa4822
elio <eliotohme@gmail.com>
2018-04-13 14:24:04 +0300

    update installation

--- srvrstp_ubun16.sh
+++ srvrstp_ubun16.sh
@@ -1,10 +1,14 @@
 # Install python dependencies
-sudo apt install -y python-mysqldb python-tk python-scipy libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+apt install -y python-pip python-mysqldb python-tk python-scipy libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
 
-sudo apt install -y  ffmpeg libav-tools
+apt install -y  ffmpeg libav-tools
 
 # Installation if django framework was in use
-# sudo pip install pymongo celery django datetime djangorestframework markdown django-filter numpy scipy pyaudio paho-mqtt 
+#  pip install pymongo celery django datetime djangorestframework markdown django-filter numpy scipy pyaudio paho-mqtt 
 
 # Installation if flask framework was in use 
-sudo pip install pymongo celery datetime markdown numpy scipy pyaudio paho-mqtt flask flask-restful Flask-PyMongo gevent pydub matplotlib
+pip install pymongo celery datetime markdown numpy scipy pyaudio paho-mqtt flask flask-restful Flask-PyMongo gevent pydub matplotlib
+
+echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" |  tee /etc/apt/sources.list.d/bintray.rabbitmq.list
+wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | apt-key add -
+apt-get update && apt-get install rabbitmq-server
