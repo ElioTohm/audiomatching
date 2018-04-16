a16fca37eab51bb7cc875fba89cd718af586ae7e
elio <eliotohme@gmail.com>
2018-04-16 09:37:46 +0300

    updated install dependencies

--- srvrstp_ubun16.sh
+++ srvrstp_ubun16.sh
@@ -7,7 +7,7 @@ apt install -y  ffmpeg libav-tools
 #  pip install pymongo celery django datetime djangorestframework markdown django-filter numpy scipy pyaudio paho-mqtt 
 
 # Installation if flask framework was in use 
-pip install pymongo celery datetime markdown numpy scipy pyaudio paho-mqtt flask flask-restful Flask-PyMongo gevent pydub matplotlib
+pip install pymongo celery datetime markdown numpy scipy pyaudio paho-mqtt flask flask-restful Flask-PyMongo gevent pydub matplotlib requests
 
 echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" |  tee /etc/apt/sources.list.d/bintray.rabbitmq.list
 wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | apt-key add -
