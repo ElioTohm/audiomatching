5f04f0111659ded6815e739ccbce9f504c08336e
elio <eliotohme@gmail.com>
2018-04-16 13:52:11 +0300

    added demo

--- XmsMatcherLight/main.py
+++ XmsMatcherLight/main.py
@@ -110,7 +110,38 @@ def match(clientrecording):
         else :
             os.unlink(client_file_path)
 
-        
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    """
+        Set up periodic task for celerybeat
+    """
+    channels = ['']
+    sender.add_periodic_task(120.0, periodicadddata.s(), name='periodic')
+
+@celery.task()
+def periodicadddata():
+    """
+        task to be added periodically
+    """
+    clients = ['1', '2', '3', '9', '7', '4', '5', '10', '11', '15', '16', '17',
+                '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
+                '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75',
+                '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', 
+                '98', '99', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111']
+    channels = ['MBCAction', 'MBC1', 'MBC2', 'MBC3', 'MBC4', 'Muted', 'Other']
+
+    timestamp = int(time.time())
+    if timestamp % 60 < 30:
+        timestamp = timestamp - (timestamp % 60)
+    else:
+        timestamp = timestamp - (timestamp % 60) + 60
+
+    records = []
+    for client_id in clients:
+        records.append({'channel_name': random.choice(channels, p=[0.10, 0.7, 0.25, 0.2, 0.27, 0.1, 0.3]), 'client_id': client_id,
+                        'confidence': 2500, 'timestamp': timestamp})
+
+    mongo.db.records.insert_many(records)        
 
 class FingerprintRequest (Resource) :
     def post(self):
